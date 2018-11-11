3e346e1fbdf5572fe9a7f75dde46f66bb552ffc4
NawafShawan <43513334+NawafShawan@users.noreply.github.com>
2018-11-11 13:58:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,13 +4,47 @@ using namespace std;
 
 int main()
 {
-    float i;
-   cout << "C: \t" << "F: " << endl;
-   for (i=0; i=i+1;)
-   {
-       cout << i <<"\t"<< (i * 9/5) + 32 << endl;
-       if (i>29)
-        break;
-   }
+    int score;
+    cout << "Enter your score Biatch: ";
+    cin >> score;
+
+    if (score == 95, 96, 97, 98, 99, 100)
+        {
+            cout << "You got the perfect score AKA A+" << endl;
+        }
+                else if (score == 90, 91, 92, 93, 94){
+                    cout << "You got A" << endl;
+                }
+
+                else if (score == 85, 86, 87 ,88 ,89){
+                     cout << "You got B+" << endl;
+                }
+
+                else if (score == 80, 81, 82, 83, 84){
+                    cout << "You got B" << endl;
+                }
+
+                else if (score == 75, 76, 77, 78, 79){
+                    cout << "You got C+" << endl;
+                }
+
+                else if (score == 70, 71, 72, 73, 74){
+                    cout << "You got C" << endl;
+                }
+
+                else if (score == 65, 66, 67, 68, 69){
+                    cout << "You got D+" << endl;
+                }
+                else if (score == 60, 61, 62, 63, 64){
+                    cout << "You got D" << endl;
+                }
+
+
+        else {
+            cout << "You got F" << endl;
+        }
+
+
+
     return 0;
 }
