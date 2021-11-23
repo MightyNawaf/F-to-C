f8da1e430ad104f93f8dcd8664cdd8c252fb68a4
Nawaf Shawan <43513334+MightyNawaf@users.noreply.github.com>
2021-11-23 18:40:21 +0300

    Update CtoF.cpp

--- CtoF.cpp
+++ CtoF.cpp
@@ -5,12 +5,10 @@ using namespace std;
 int main()
 {
     float i;
-   cout << "C: \t" << "F: " << endl;
-   for (i=0; i=i+1;)
-   {
-       cout << i <<"\t"<< (i * 9/5) + 32 << endl;
-       if (i>29)
-        break;
-   }
+    cout << "C: \t" << "F: " << endl;
+    for (i=0; i<31; i++)
+    {
+        cout << i <<"\t"<< (i * 9/5) + 32 << endl;
+    }
     return 0;
 }
